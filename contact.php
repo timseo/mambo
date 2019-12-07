9201d2f5ee018834b778e46a72660186b3e498d3
Arul Ananth <php.jquery.1988@gmail.com>
2019-12-07 10:18:53 +0300

    validation fixed

--- contact.php
+++ contact.php
@@ -16,7 +16,7 @@ $conf_subject = 'Thanks for reaching out to Mambo';
 // Who should the confirmation email be from?
 $conf_sender = 'Mambo Italian Street Food <mambostreet@gmail.com>';
 
-$msg = $_POST['Name'] . ",\n\nThank you for your recent enquiry. A member of our
+$msg = $_POST['username'] . ",\n\nThank you for your recent enquiry. A member of our
 team will respond to your message as soon as possible. If you do not hear from us please give us a call. (770) 485-4026";
 
 mail( $_POST['email'], $conf_subject, $msg, 'From: ' . $conf_sender );
